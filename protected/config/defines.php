baa02de20b5d57a8ced4942b8bc1432f3df8d265
Alan Willms <dyulax@gmail.com>
2013-11-16 16:32:28 -0200

    Remove arquivos de configuração que foram versionados por engano

--- protected/config/defines.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-date_default_timezone_set('America/Sao_Paulo');
-
-defined('CAMINHO_FRAMEWORK') or define('CAMINHO_FRAMEWORK', '/home/juliano/www/dengue/protected/framework/');
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',1);
-?>
