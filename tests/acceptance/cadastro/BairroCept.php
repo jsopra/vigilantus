f19357a34b91b047caf2f5d3c56ce64c3bbdb7ef
Alan Willms <dyulax@gmail.com>
2014-01-17 16:34:07 -0200

    Adiciona teste do CRUD de bairros

---
+++ tests/acceptance/cadastro/BairroCept.php
@@ -0,0 +1,34 @@
+<?php
+
+$eu = new CaraDaWeb($scenario);
+$eu->quero('verificar que o CRUD de bairros funciona');
+$eu->facoLoginComo('administrador', 'administrador');
+$eu->clico('Cadastro');
+$eu->clico('Bairros');
+
+$eu->espero('cadastrar um bairro');
+$eu->clico('Cadastrar Bairro');
+$eu->vejoNoTitulo('Cadastrar Bairro');
+$eu->selecionoOpcao('Município', 'Tapera');
+$eu->selecionoOpcao('Tipo de Bairro', 'Rural');
+$eu->preenchoCampo('Nome', 'Passo de los Fuertes');
+$eu->clico('Cadastrar');
+$eu->aguardoPor(1);
+$eu->vejo('O cadastro foi realizado com sucesso.');
+
+$eu->espero('editar um bairro');
+$eu->clicoNoGrid('Passo de los Fuertes', 'Atualizar');
+$eu->vejoNoTitulo('Atualizar Bairro');
+$eu->selecionoOpcao('Município', 'Chapecó');
+$eu->selecionoOpcao('Tipo de Bairro', 'Urbano');
+$eu->preenchoCampo('Nome', 'Passo dos Fortes');
+$eu->clico('Atualizar');
+$eu->aguardoPor(1);
+$eu->vejo('O registro foi atualizado com sucesso.');
+
+$eu->espero('excluir um bairro');
+$eu->clicoNoGrid('Passo dos Fortes', 'Excluir');
+$eu->vejoNaPopUp('Tem certeza de que deseja excluir este item?');
+$eu->aceitoPopUp();
+$eu->aguardoPor(1);
+$eu->naoVejo('Passo dos Fortes');
