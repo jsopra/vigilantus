8d165b73c6a1375db19acda5ad9c7b0062736601
Juliano Baggio di Sopra <jsopra@gmail.com>
2014-01-22 19:17:29 -0200

    corrigindo cagada em modelos

--- models/BoletimRgFechamento.php
+++ models/BoletimRgFechamento.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\models;
+use app\components\ActiveRecord;
 
 /**
  * This is the model class for table "boletim_rg_fechamento".
@@ -10,11 +11,12 @@ namespace app\models;
  * @property integer $boletim_rg_id
  * @property integer $condicao_imovel_id
  * @property integer $quantidade
+ * @property integer $municipio_id
  *
  * @property BoletinsRg $boletimRg
  * @property ImovelCondicoes $condicaoImovel
  */
-class BoletimRgFechamento extends \yii\db\ActiveRecord
+class BoletimRgFechamento extends ActiveRecord
 {
 	/**
 	 * @inheritdoc
@@ -30,9 +32,9 @@ class BoletimRgFechamento extends \yii\db\ActiveRecord
 	public function rules()
 	{
 		return [
-			[['data', 'boletim_rg_id', 'condicao_imovel_id'], 'required'],
+			[['data', 'boletim_rg_id', 'condicao_imovel_id', 'municipio_id'], 'required'],
 			[['data'], 'safe'],
-			[['boletim_rg_id', 'condicao_imovel_id', 'quantidade'], 'integer']
+			[['boletim_rg_id', 'condicao_imovel_id', 'quantidade', 'municipio_id'], 'integer']
 		];
 	}
 
@@ -47,6 +49,7 @@ class BoletimRgFechamento extends \yii\db\ActiveRecord
 			'boletim_rg_id' => 'Boletim Rg ID',
 			'condicao_imovel_id' => 'Condicao Imovel ID',
 			'quantidade' => 'Quantidade',
+            'municipio_id' => 'Município',
 		];
 	}
 
@@ -65,4 +68,12 @@ class BoletimRgFechamento extends \yii\db\ActiveRecord
 	{
 		return $this->hasOne(ImovelCondicoes::className(), ['id' => 'condicao_imovel_id']);
 	}
+    
+    /**
+     * @return Municipio
+     */
+    public function getMunicipio()
+    {
+        return $this->hasOne(Municipio::className(), ['id' => 'municipio_id']);
+    }
 }
