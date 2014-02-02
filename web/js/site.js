992810932f320841dc21ffd81a179a46ac8051c3
Cristian de Oliveira <crisdeoliveira.si@gmail.com>
2014-02-02 08:36:49 -0200

    criada funcao para fechar automaticamente o alerta, quando tiver a class alert-success :D

--- web/js/site.js
+++ web/js/site.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    $('a[rel="tooltip"]').tooltip({placement: 'right'});
+	$('a[rel="tooltip"]').tooltip({placement: 'right'});
+
+	function createAutoClosingAlert(selector, delay) {
+		var alert = $(selector).alert();
+		window.setTimeout(function() { alert.alert('close') }, delay);
+	}
+
+	if ($('.alert-success').length) {
+		createAutoClosingAlert(".alert-success", 2000);
+	}
 })
