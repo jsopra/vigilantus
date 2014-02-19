f2be51b7d79bb12fb4a8e1cdb7dd280f15543da4
Juliano Baggio di Sopra <jsopra@gmail.com>
2014-02-19 08:29:49 -0300

    Inicio da remoção da tabela e relacioamentos

---
+++ migrations/m140219_111233_remover_tipo_imovel.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Schema;
+
+class m140219_111233_remover_tipo_imovel extends \yii\db\Migration
+{
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m140219_111233_remover_tipo_imovel cannot be reverted.\n";
+		return false;
+	}
+}
