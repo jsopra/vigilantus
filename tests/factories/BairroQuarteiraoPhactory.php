15a290e55dcd7949e7e190faa7b28f3023fb6293
Alan Willms <dyulax@gmail.com>
2014-03-26 11:09:09 -0300

    Corrige BairroQuarteiraoTest

--- tests/factories/BairroQuarteiraoPhactory.php
+++ tests/factories/BairroQuarteiraoPhactory.php
@@ -8,6 +8,7 @@ class BairroQuarteiraoPhactory
             'numero_quarteirao' => '#{sn}',
             'municipio_id' => Phactory::hasOne('municipio'),
             'bairro_id' => Phactory::hasOne('bairro'),
+            'inserido_por' => Phactory::hasOne('usuario'),
         ];
     }
 }
