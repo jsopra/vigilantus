41fc5ee98e2c28f69687ce1410ed144680637459
Juliano Baggio di Sopra <jsopra@gmail.com>
2014-07-29 11:18:21 -0300

    Inicio migrate

---
+++ migrations/m140729_141216_quarteirao_id_no_foco.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m140729_141216_quarteirao_id_no_foco extends Migration
+{
+    public function safeUp()
+    {
+        $this->addColumn('focos_transmissores', 'bairro_quarteirao_id', 'integer references bairro_quarteiroes(id)');
+        
+        $this->execute("
+            UPDATE focos_transmissores SET bairro_quarteirao_id = (
+                SELECT id 
+                FROM bairro_quarteirao
+                WHERE 
+            )
+        ");
+    }
+
+    public function safeDown()
+    {
+        echo "m140729_141216_quarteirao_id_no_foco cannot be reverted.\n";
+        return false;
+    }
+}
