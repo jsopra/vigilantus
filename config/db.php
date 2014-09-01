7d49561aee619660bc205218b9a1bc41b0c90777
Juliano Baggio di Sopra <jsopra@gmail.com>
2014-09-01 20:51:58 -0300

    dividindo dsn

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => getenv('VIGILANTUS_DB_DSN'),
+    'dsn' => getenv('VIGILANTUS_DB_DSN_HOST') . ';' . getenv('VIGILANTUS_DB_DSN_DBNAME'),
     'username' => getenv('VIGILANTUS_DB_USERNAME'),
     'password' => getenv('VIGILANTUS_DB_PASSWORD'),
     'charset' => 'utf8',
