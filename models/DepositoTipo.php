9e46fbc15f5799b91029b3648eb7da24a312267d
Juliano Baggio di Sopra <jsopra@gmail.com>
2015-03-19 20:10:52 -0300

    Coluna com sigla e descrição de tipo de deposito para facilitar inserção via formulário

--- models/DepositoTipo.php
+++ models/DepositoTipo.php
@@ -21,8 +21,9 @@ use app\components\ClienteActiveRecord;
  *
  * @property DepositoTipos $depositoTipoPai
  */
-class DepositoTipo extends ClienteActiveRecord 
+class DepositoTipo extends ClienteActiveRecord
 {
+
 	/**
 	 * @inheritdoc
 	 */
@@ -74,7 +75,7 @@ class DepositoTipo extends ClienteActiveRecord
 	{
 		return $this->hasOne(DepositoTipo::className(), ['id' => 'deposito_tipo_pai']);
 	}
-    
+
     /**
      * @return \yii\db\ActiveRelation
      */
@@ -89,5 +90,10 @@ class DepositoTipo extends ClienteActiveRecord
     public function getAtualizadoPor()
     {
         return $this->hasOne(Usuario::className(), ['id' => 'atualizado_por']);
+    }
+
+    public function getDescricao_sigla()
+    {
+        return '(' . $this->sigla . ') ' . $this->descricao;
     }
 }
