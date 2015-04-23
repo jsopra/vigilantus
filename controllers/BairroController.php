38f566549bb6ece675d46fcb349448a9818ed4ca
Juliano Baggio di Sopra <jsopra@gmail.com>
2015-04-23 08:11:20 -0300

    Corrigindo bug na edição da área de um bairro

--- controllers/BairroController.php
+++ controllers/BairroController.php
@@ -3,8 +3,24 @@
 namespace app\controllers;
 
 use app\components\CRUDController;
+use app\helpers\GoogleMapsAPIHelper;
 
 class BairroController extends CRUDController
 {
-    
+    public function actionUpdate($id)
+    {
+        $model = is_object($id) ? $id : $this->findModel($id);
+
+        if (!$this->loadAndSaveModel($model, $_POST, ['index'])) {
+
+            $model->loadCoordenadas();
+            if(!$model->getIsNewRecord() && !$model->coordenadasJson && $model->coordenadas) {
+                $model->coordenadasJson = GoogleMapsAPIHelper::arrayToCoordinatesJson($model->coordenadas);
+            }
+
+            return $this->renderAjaxOrLayout('update', [
+                'model' => $model,
+            ]);
+        }
+    }
 }
--- controllers/BairroQuarteiraoController.php
+++ controllers/BairroQuarteiraoController.php
@@ -12,11 +12,11 @@ class BairroQuarteiraoController extends DependentCRUDController
 {
     protected $dependentModel = 'Bairro';
     protected $parentField = 'bairro_id';
- 
+
     private $_municipio;
     private $_bairro;
     private $_quarteiroes;
-    
+
     public function actionIndex()
     {
         $municipio = $this->parentObject->municipio;
@@ -24,25 +24,25 @@ class BairroQuarteiraoController extends DependentCRUDController
             Yii::$app->session->setFlash('error', 'Município não tem coordenadas geográficas definidas');
             $this->redirect(['bairro/index']);
         }
-        
+
         return parent::actionIndex();
     }
-    
+
     public function actionCreate()
     {
         $model = $this->buildNewModel();
 
         $parentField = $this->parentField;
         $model->$parentField = $this->parentObject->id;
-        
-        if (!$this->loadAndSaveModel($model, $_POST, ['index', 'parentID' => $this->dependentID])) { 
+
+        if (!$this->loadAndSaveModel($model, $_POST, ['index', 'parentID' => $this->dependentID])) {
             $this->_init($model);
             $this->_quarteiroes = BairroQuarteirao::find()->doBairro($this->_bairro->id)->comCoordenadas();
-            
+
             return $this->renderAjaxOrLayout('create', [
-                'model' => $model, 
-                'municipio' => $this->_municipio, 
-                'bairro' => $this->_bairro, 
+                'model' => $model,
+                'municipio' => $this->_municipio,
+                'bairro' => $this->_bairro,
                 'quarteiroes' => $this->_quarteiroes,
                 'coordenadasQuarteiroes' => BairroQuarteirao::getCoordenadas($this->_quarteiroes)
             ]);
@@ -51,36 +51,37 @@ class BairroQuarteiraoController extends DependentCRUDController
 
     public function actionUpdate($id)
     {
-        
+
         $model = is_object($id) ? $id : $this->findModel($id);
-        
+
         $parentField = $this->parentField;
         $model->$parentField = $this->parentObject->id;
-        
-        if (!$this->loadAndSaveModel($model, $_POST, ['index', 'parentID' => $this->dependentID])) { 
-            
+
+        if (!$this->loadAndSaveModel($model, $_POST, ['index', 'parentID' => $this->dependentID])) {
+
             $this->_init($model);
             $this->_quarteiroes = BairroQuarteirao::find()->queNao($model->id)->doBairro($this->_bairro->id)->comCoordenadas();
             $model->loadCoordenadas();
-            if(!$model->getIsNewRecord() && !$model->coordenadasJson && $model->coordenadas) 
+            if(!$model->getIsNewRecord() && !$model->coordenadasJson && $model->coordenadas) {
                 $model->coordenadasJson = GoogleMapsAPIHelper::arrayToCoordinatesJson($model->coordenadas);
-            
+            }
+
             return $this->renderAjaxOrLayout('update', [
-                'model' => $model, 
-                'municipio' => $this->_municipio, 
-                'bairro' => $this->_bairro, 
+                'model' => $model,
+                'municipio' => $this->_municipio,
+                'bairro' => $this->_bairro,
                 'quarteiroes' => $this->_quarteiroes,
                 'coordenadasQuarteiroes' => BairroQuarteirao::getCoordenadas($this->_quarteiroes)
-            ]);    
+            ]);
         }
     }
-    
+
     private function _init($model) {
-        
+
         $this->_bairro = $this->parentObject;
         $this->_bairro->loadCoordenadas();
-        
+
         $this->_municipio = $this->_bairro->municipio;
-        $this->_municipio->loadCoordenadas();        
+        $this->_municipio->loadCoordenadas();
     }
 }
