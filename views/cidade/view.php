143d0973c72eb9af5604fa7469c9678588fbef40
Juliano Baggio di Sopra <jsopra@gmail.com>
2015-09-13 09:24:34 -0300

    acertando estatistica e sugestões de interface

--- views/cidade/view.php
+++ views/cidade/view.php
@@ -5,15 +5,18 @@ use yii\helpers\Url;
 
 $this->title = 'Ocorrências – ' . $municipio->nome . '/' . $municipio->sigla_estado;
 ?>
+
 <h1 class="text-center">
     <?= MunicipioHelper::getBrasaoAsImageTag($municipio, 'small'); ?>
     <a href="<?= Url::to(['cidade/index', 'id' => $cliente->id]); ?>">
         <?= Html::encode($municipio->nome . '/' . $municipio->sigla_estado) ?>
     </a>
 </h1>
-<p class="text-center">
-    Contamos com a sua ajuda para tornar o nosso município melhor.
+
+<p class="text-center" style="line-height: 1.5em; color: #585858; font-size: 1.6em;">
+    Contamos com a <font style="font-weight: bold; font-size: 1.05em; color: #000;">sua ajuda</font> para tornar o nosso <font style="font-weight: bold; font-size: 1.05em; color: #000;">município melhor!</font>
 </p>
+
 <p class="text-center bloco-botoes-ocorrencias">
     <a href="<?= Url::to(['registrar-ocorrencia/index', 'id' => $cliente->id]) ?>" class="btn btn-danger btn-lg">
         <i class="fa fa-plus"></i>
@@ -24,11 +27,13 @@ $this->title = 'Ocorrências – ' . $municipio->nome . '/' . $municipio->sigla_
         acompanhar ocorrência
     </a>
 </p>
+
 <div class="bloco-numero-ocorrencias text-center">
     <p><strong class="recebidas"><?= $numeroOcorrenciasRecebidas ?></strong> ocorrências recebidas,</p>
-    <p><strong class="atendidas"><?= $percentualOcorrenciasAtendidas ?>%</strong> já foram atendidas.</p>
+    <p><strong class="atendidas"><?= $percentualOcorrenciasAtendidas ?>%</strong> já foram finalizadas.</p>
     <p class="fonte-informacao">(informações coletadas desde <?= $dataPrimeiraOcorrencia ?>)</p>
 </div>
+
 <div class="panel panel-default text-center">
     <div class="panel-heading mapa-focos-chamada">
         <p>
