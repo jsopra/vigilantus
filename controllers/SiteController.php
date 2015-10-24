5eb0dcf0cb448398e5acd42e105633118c3b726a
Alan Willms <dyulax@gmail.com>
2015-10-24 12:08:55 -0200

    Adiciona redirecionamento para os links quebrados de municípios

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -229,10 +229,36 @@ class SiteController extends Controller
             $code = $exception->getCode();
         }
 
+        // @FIXME remover este bloco de redirecionamentos após 31/dez/2015
+        // 301 = movido permanentemente, então o Google nunca mais apontará
+        // para a URL antiga
+        switch (Yii::$app->request->url) {
+            case '/blumenau' :
+                return $this->redirect('/blumenau-sc', 301);
+            case '/cananeia' :
+                return $this->redirect('/cananeia-sp', 301);
+            case '/chapeco':
+                return $this->redirect('/chapeco-sc', 301);
+            case '/floripa':
+                return $this->redirect('/florianopolis-sc', 301);
+            case '/itajai':
+                return $this->redirect('/itajai-sc', 301);
+            case '/maravilha':
+                return $this->redirect('/maravilha-sc', 301);
+            case '/rondonopolis':
+                return $this->redirect('/rondonopolis-mt', 301);
+            case '/smo':
+                return $this->redirect('/sao-miguel-do-oeste-sc', 301);
+            case '/to':
+                return $this->redirect('/teofilo-otoni-mg', 301);
+            case '/ubirajara' :
+                return $this->redirect('/ubirajara-sp', 301);
+        }
+        // @FIXME fim do bloco a ser removido
+
         if ($exception instanceof Exception) {
             $name = $exception->getName();
-        }
-        else {
+        } else {
             $name = $this->defaultName ?: Yii::t('yii', 'Error');
         }
 
@@ -248,8 +274,7 @@ class SiteController extends Controller
 
         if (Yii::$app->request->isAjax) {
             return "$name: $message";
-        }
-        else {
+        } else {
             return $this->render('error', [
                 'name' => $name,
                 'message' => $message,
