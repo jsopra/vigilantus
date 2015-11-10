8eae7b4a414d06d3152f096e7aef1d8a643b8a86
Juliano Baggio di Sopra <jsopra@gmail.com>
2015-11-10 08:23:44 -0200

    Acertando ferramenta de bairro

--- controllers/BairroController.php
+++ controllers/BairroController.php
@@ -3,10 +3,27 @@
 namespace app\controllers;
 
 use app\components\CRUDController;
-use app\helpers\GoogleMapsAPIHelper;
+use app\helpers\MapHelper;
 
 class BairroController extends CRUDController
 {
+
+    public function actionCreate()
+    {
+        $model = $this->buildNewModel();
+
+        if (!$this->loadAndSaveModel($model, $_POST, ['index'])) {
+
+            if ($model->coordenadasJson && !$model->coordenadas) {
+                $model->coordenadas = MapHelper::jsonToCoordinatesArray($model->coordenadasJson);
+            }
+
+            return $this->renderAjaxOrLayout('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
     public function actionUpdate($id)
     {
         $model = is_object($id) ? $id : $this->findModel($id);
@@ -14,8 +31,10 @@ class BairroController extends CRUDController
         if (!$this->loadAndSaveModel($model, $_POST, ['index'])) {
 
             $model->loadCoordenadas();
-            if(!$model->getIsNewRecord() && !$model->coordenadasJson && $model->coordenadas) {
-                $model->coordenadasJson = GoogleMapsAPIHelper::arrayToCoordinatesJson($model->coordenadas);
+            if (!$model->getIsNewRecord() && !$model->coordenadasJson && $model->coordenadas) {
+                $model->coordenadasJson = MapHelper::getArrayCoordenadas($model->coordenadas);
+            } else if($model->coordenadasJson) {
+                $model->coordenadas = MapHelper::jsonToCoordinatesArray($model->coordenadasJson);
             }
 
             return $this->renderAjaxOrLayout('update', [
