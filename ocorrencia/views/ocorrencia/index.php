75c99cd94f6fefad7cfa5006c145ce8c3926d1b0
Theylor Fiorentin <theylor@unochapeco.edu.br>
2017-08-17 14:21:05 -0300

    Foi criado ferramenta para encaminhar denúncias para outros setores

--- ocorrencia/views/ocorrencia/index.php
+++ ocorrencia/views/ocorrencia/index.php
@@ -198,7 +198,7 @@ $diasVemelho = Configuracao::getValorConfiguracaoParaCliente(Configuracao::ID_QU
 			[
 				'header' => 'Ações',
                 'class' => 'app\components\OcorrenciaColumn',
-                'template' => '{detalhes} {aprovar} {reprovar} {mudar-status} {anexo} {tentativa-averiguacao} {comprovante} {delete}',
+                'template' => '{detalhes} {aprovar} {reprovar} {mudar-status} {mudar-setor} {anexo} {tentativa-averiguacao} {comprovante} {delete}',
             ],
 		],
 	]); ?>
