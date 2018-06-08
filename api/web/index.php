cc165d762c328b8f5bb7ae6ecbab1f00f0ccc0c0
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-06-07 21:48:53 -0300

    api ajuste

--- api/web/index.php
+++ api/web/index.php
@@ -1,4 +1,7 @@
 <?php
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../config/env.php');
+
 $env = getenv('ENVIRONMENT');
 
 if ($env == 'test') {
@@ -12,7 +15,6 @@ if ($env == 'test') {
     defined('YII_ENV') or define('YII_ENV', 'prod');
 }
 
-require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../config/main.php');
 
