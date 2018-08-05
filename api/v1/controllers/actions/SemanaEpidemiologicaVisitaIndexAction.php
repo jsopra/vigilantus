5bcbf3b3e950743f30a998c496d7ba669d7f94b2
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-08-05 13:22:05 -0300

    Ajustando APIS

---
+++ api/v1/controllers/actions/SemanaEpidemiologicaVisitaIndexAction.php
@@ -0,0 +1,50 @@
+<?php
+namespace api\v1\controllers\actions;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\rest\Action;
+use app\models\EquipeAgente;
+
+class SemanaEpidemiologicaVisitaIndexAction extends Action
+{
+    public $prepareDataProvider;
+
+    /**
+     * @return ActiveDataProvider
+     */
+    public function run()
+    {
+        if ($this->checkAccess) {
+            call_user_func($this->checkAccess, $this->id);
+        }
+
+        return $this->prepareDataProvider();
+    }
+
+    /**
+     * Prepares the data provider that should return the requested collection of the models.
+     * @return ActiveDataProvider
+     */
+    protected function prepareDataProvider()
+    {
+        if ($this->prepareDataProvider !== null) {
+            return call_user_func($this->prepareDataProvider, $this);
+        }
+
+        $modelClass = $this->modelClass;
+
+        $query = $modelClass::find();
+
+        $user = Yii::$app->user->identity;
+        $agente = EquipeAgente::find()->doUsuario($user->id)->one();
+        if ($agente instanceof EquipeAgente) {
+            $query->doAgente($agente->id);
+        }
+
+        return Yii::createObject([
+            'class' => ActiveDataProvider::className(),
+            'query' => $query
+        ]);
+    }
+}
