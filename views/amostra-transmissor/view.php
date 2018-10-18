a8247c9e9f250ea3248945e7966ee5ea0aa2d2c8
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-10-18 02:25:26 -0300

    visita imvel tratamento

--- views/amostra-transmissor/view.php
+++ views/amostra-transmissor/view.php
@@ -29,6 +29,11 @@ $this->params['breadcrumbs'][] = $this->title;
             'value' => $model->getFormattedAttribute('data_coleta'),
         ],
         [
+            'attribute' => 'tipo_deposito_id',
+            'filter' => DepositoTipo::listData('descricao'),
+            'value' => $model->tipo_deposito_id ? ($model->tipoDeposito && $model->tipoDeposito->sigla ? $model->tipoDeposito->sigla : $model->tipoDeposito->descricao) : null,
+        ],
+        [
             'attribute' => 'quarteirao_id',
             'value' => $model->bairroQuarteirao->numero_sequencia,
         ],
