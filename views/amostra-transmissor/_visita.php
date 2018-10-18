e90b1fd7119d5042a541622004d3c1e2af4f4c56
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-10-18 02:11:36 -0300

    visita imvel tratamento

--- views/amostra-transmissor/_visita.php
+++ views/amostra-transmissor/_visita.php
@@ -5,17 +5,27 @@ use yii\helpers\Html;
 use \yii\web\View;
 use yii\widgets\DetailView;
 use Yii\helpers\Url;
-
-
-$this->title = 'Visita em Imóvel';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<h1><?= Html::encode($this->title) ?></h1>
 
 <?= DetailView::widget([
     'model' => $model,
     'attributes' => [
-       
+        'semana_epidemiologica_visita_id',
+        'visita_atividade_id',
+        'rua_id',
+        'quarteirao_id',
+        'logradouro',
+        'numero',
+        'sequencia',
+        'complemento',
+        'tipo_imovel_id',
+        'hora_entrada',
+        'visita_tipo',
+        'pendencia',
+        'depositos_eliminados',
+        'numero_amostra_inicial',
+        'numero_amostra_final',
+        'quantidade_tubitos',
     ]
 
 ]) ?>
