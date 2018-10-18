014af39a028298e889e7800a4ed59d62252cbf88
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-10-18 02:09:35 -0300

    visita imvel tratamento

--- views/amostra-transmissor/_visita.php
+++ views/amostra-transmissor/_visita.php
@@ -5,13 +5,9 @@ use yii\helpers\Html;
 use \yii\web\View;
 use yii\widgets\DetailView;
 use Yii\helpers\Url;
-use app\models\DepositoTipo;
-use kartik\select2\Select2;
-use app\models\EspecieTransmissor;
-use app\models\ImovelTipo;
 
 
-$this->title = 'Análise Laboratorial';
+$this->title = 'Visita em Imóvel';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h1><?= Html::encode($this->title) ?></h1>
@@ -19,98 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <?= DetailView::widget([
     'model' => $model,
     'attributes' => [
-        'id',
-        [
-            'attribute' => 'data_criacao',
-            'value' => $model->getFormattedAttribute('data_criacao'),
-        ],
-        [
-            'attribute' => 'data_coleta',
-            'value' => $model->getFormattedAttribute('data_coleta'),
-        ],
-        [
-            'attribute' => 'tipo_deposito_id',
-            'filter' => DepositoTipo::listData('descricao'),
-            'value' => $model->tipo_deposito_id && $model->tipoDeposito->sigla ? $model->tipoDeposito->sigla : $model->tipoDeposito->descricao,
-        ],
-        [
-            'attribute' => 'quarteirao_id',
-            'value' => $model->bairroQuarteirao->numero_sequencia,
-        ],
-        'endereco',
-        'observacoes',
-        'numero_casa',
-        'numero_amostra',
-        'quantidade_larvas',
-        'quantidade_pupas',
-        [
-            'attribute' => 'foco_transmissor_id',
-            'format' => 'raw',
-            'value' => $model->foco_transmissor_id ? Html::a(
-                'Ver foco',
-                ['/mapa/tratamento-foco', 'TratamentoFocoMapForm[foco_id]' => $model->foco_transmissor_id],
-                ['class'=>'link']
-            ) : null,
-        ],
+       
     ]
 
 ]) ?>
-
-
-<?php if($model->foco === null || $submitting) : ?>
-<?php $form = ActiveForm::begin(); ?>
-    <div class="row">
-        <div class="col-xs-2">
-            <?= $form->field($model, 'foco')->dropDownList([0 => 'Não', 1 => 'Sim'], ['prompt' => 'Selecione…']) ?>
-        </div>
-    </div>
-
-    <div class="row focoTrue">
-        <div class="col-xs-3">
-            <?= $form->field($model, 'especie_transmissor_id')->widget(
-                Select2::classname(),
-                [
-                    'data' => ['' => ''] + EspecieTransmissor::listData('nome'),
-                    'pluginOptions' => [
-                        'allowClear' => true
-                    ],
-                ]
-            );
-            ?>
-        </div>
-        <div class="col-xs-3">
-            <?= $form->field($model, 'planilha_imovel_tipo_id')->widget(
-                Select2::classname(),
-                [
-                    'data' => ['' => ''] + ImovelTipo::listData('descricao_sigla'),
-                    'pluginOptions' => [
-                        'allowClear' => true
-                    ],
-                ]
-            );
-            ?>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-xs-12">
-            <?= $form->field($model, 'observacoes')->textarea(['rows' => 5]) ?>
-        </div>
-    </div>
-
-    <div class="form-group form-actions">
-    <?php
-        echo Html::submitButton(
-            $model->isNewRecord ? 'Salvar' : 'Concluir Análise',
-            ['class' => $model->isNewRecord ? 'btn btn-flat success' : 'btn btn-flat primary']
-    );
-?>
-<?php else : ?>
-    <?php
-        echo Html::a(
-            'Voltar',
-            array('/amostra-transmissor/index'),
-            array('class'=>'btn btn-flat success','rel'=>'tooltip','data-role'=>'cancel','data-title'=>'Ir à lista de Amostra Transmissors')
-        );
-    ?>
-<?php endif; ?>
