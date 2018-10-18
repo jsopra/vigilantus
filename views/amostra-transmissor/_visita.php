f1019cc1d706519d95e633d77d9af627a7280a29
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-10-18 02:18:37 -0300

    visita imvel tratamento

--- views/amostra-transmissor/_visita.php
+++ views/amostra-transmissor/_visita.php
@@ -5,22 +5,43 @@ use yii\helpers\Html;
 use \yii\web\View;
 use yii\widgets\DetailView;
 use Yii\helpers\Url;
+use app\models\VisitaAtividade;
+use app\models\VisitaTipo;
 ?>
 
 <?= DetailView::widget([
     'model' => $model,
     'attributes' => [
-        'semana_epidemiologica_visita_id',
-        'visita_atividade_id',
-        'rua_id',
-        'quarteirao_id',
+        [
+            'attribute' => 'visita_atividade_id',
+            'filter' => false,
+            'value' => $model->visita_atividade_id ? VisitaAtividade::getDescricao($model->visita_atividade_id) : null,
+        ],
+        [
+            'attribute' => 'quarteirao_id',
+            'filter' => false,
+            'value' => $model->quarteirao_id ? $model->quarteirao->numero_quarteirao : null,
+        ],
+        [
+            'header' => 'Bairro',
+            'filter' => false,
+            'value' => $model->quarteirao_id ? $model->quarteirao->bairro->nome : null,
+        ],
         'logradouro',
         'numero',
         'sequencia',
         'complemento',
-        'tipo_imovel_id',
+        [
+            'attribute' => 'tipo_imovel_id',
+            'filter' => false,
+            'value' => $model->tipo_imovel_id ? $model->tipoImovel->nome : null,
+        ],
         'hora_entrada',
-        'visita_tipo',
+        [
+            'attribute' => 'visita_tipo',
+            'filter' => false,
+            'value' => $model->visita_tipo ? VisitaTipo::getDescricao($model->visita_tipo) : null,
+        ],
         'pendencia',
         'depositos_eliminados',
         'numero_amostra_inicial',
