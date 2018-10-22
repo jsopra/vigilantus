6fdffd07601076ab928c21126e2b3bba54e1d6c6
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-10-21 23:35:34 -0300

    iniciando relatorio

--- views/semana-epidemiologica/_resumoTrabalho.php
+++ views/semana-epidemiologica/_resumoTrabalho.php
@@ -40,22 +40,24 @@
 			</thead>
 			<tbody>
 				<tr>
-					<table class="table table-striped table-bordered">
-						<thead>
-							<tr>
-								<?php foreach ($data['imoveis'] as $label => $value) : ?>
-									<th><?= $label; ?></th>
-								<?php endforeach; ?>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<?php foreach ($data['imoveis'] as $label => $value) : ?>
-									<th><?= $value; ?></th>
-								<?php endforeach; ?>
-							</tr>
-						</tbody>
-					</table>
+					<td>
+						<table class="table table-striped table-bordered">
+							<thead>
+								<tr>
+									<?php foreach ($data['imoveis'] as $label => $value) : ?>
+										<th><?= $label; ?></th>
+									<?php endforeach; ?>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<?php foreach ($data['imoveis'] as $label => $value) : ?>
+										<th><?= $value; ?></th>
+									<?php endforeach; ?>
+								</tr>
+							</tbody>
+						</table>
+					</td>
 				</tr>
 			</tbody>
 		</table>
