e25101657aec711ece29a2d39115bf4548ba0ba0
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-10-21 22:58:42 -0300

    iniciando relatorio

--- views/semana-epidemiologica/_resumoTrabalho.php
+++ views/semana-epidemiologica/_resumoTrabalho.php
@@ -72,10 +72,62 @@
 		</table>
     </div>
     <div class="col-xs-2">
-    	2
+    	<table class="table">
+			<thead>
+				<tr>
+					<th>Pendências</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<table class="table">
+						<thead>
+							<tr>
+								<?php foreach ($data['pendencias'] as $label => $value) : ?>
+									<th><?= $label; ?></th>
+								<?php endforeach; ?>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<?php foreach ($data['pendencias'] as $label => $value) : ?>
+									<th><?= $value; ?></th>
+								<?php endforeach; ?>
+							</tr>
+						</tbody>
+					</table>
+				</tr>
+			</tbody>
+		</table>
     </div>
     <div class="col-xs-3">
-    	3
+    	<table class="table">
+			<thead>
+				<tr>
+					<th>Nº Depósitos inspecionados por tipo</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<table class="table">
+						<thead>
+							<tr>
+								<?php foreach ($data['depositos_inspecionados'] as $label => $value) : ?>
+									<th><?= $label; ?></th>
+								<?php endforeach; ?>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<?php foreach ($data['depositos_inspecionados'] as $label => $value) : ?>
+									<th><?= $value; ?></th>
+								<?php endforeach; ?>
+							</tr>
+						</tbody>
+					</table>
+				</tr>
+			</tbody>
+		</table>
     </div>
 </div>
 <div class="row">
