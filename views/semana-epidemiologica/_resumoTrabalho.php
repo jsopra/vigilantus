5456ea238c1cee671afcfdf38545d06a387552e2
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-10-21 22:51:03 -0300

    iniciando relatorio

--- views/semana-epidemiologica/_resumoTrabalho.php
+++ views/semana-epidemiologica/_resumoTrabalho.php
@@ -1,12 +1,28 @@
-<table>
-	<tr>
-		<td>
-			lum
-		</td>
-	</tr>
-	<tr>
-		<td>
-			ldois
-		</td>
-	</tr>
-</table>
+<div class="row">
+    <div class="col-xs-3">
+    	3
+    </div>
+    <div class="col-xs-3">
+    	3
+    </div>
+    <div class="col-xs-1">
+    	1
+    </div>
+    <div class="col-xs-2">
+    	2
+    </div>
+    <div class="col-xs-3">
+    	3
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-5">
+    	5
+    </div>
+    <div class="col-xs-1">
+    	1
+    </div>
+    <div class="col-xs-6">
+    	6
+    </div>
+ </div>
