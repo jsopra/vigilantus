c49100641294b0294c9e4c8b43ef013374d8ac5f
Juliano Baggio di Sopra <jsopra@gmail.com>
2018-12-31 19:33:44 -0200

    ajustando query

--- models/query/redis/FechamentoRgQuery.php
+++ models/query/redis/FechamentoRgQuery.php
@@ -50,6 +50,7 @@ class FechamentoRgQuery extends RedisActiveQuery
         {
             $ids[] = $record->quarteirao_id;
         }
+        $this->andWhere(['imovel_tipo_id' => 1]);
         //$this->andWhere(['in', 'bairro_quarteirao_id', $ids]);
         return $this;
     }
